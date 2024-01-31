4de24cd79aad25d8d9504a8296c8458eb54fd952
Jordan <jordanwatson000@gmail.com>
2024-01-31 17:34:33 +0000

    basic setup

---
+++ supabase/functions/music/index.ts
@@ -0,0 +1,34 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import express from 'npm:express@4.18.2'
+
+const app = express()
+app.use(express.json())
+const port = 3000
+
+app.get('/hello-world', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/hello-world', (req, res) => {
+  const { name } = req.body
+  res.send(`Hello ${name}!`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/music' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
