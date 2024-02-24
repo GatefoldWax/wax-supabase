904a9f112c457699e2dc7cfaf3e6caa1457d6ca7
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-02-24 14:46:06 +0000

    fix type errors in user route

--- supabase/functions/users/index.ts
+++ supabase/functions/users/index.ts
@@ -1,9 +1,9 @@
 import express, {
-  NextFunction,
-  Request,
-  Response,
-  Router,
-  ErrorRequestHandler,
+	NextFunction,
+	Request,
+	Response,
+	Router,
+	ErrorRequestHandler,
 } from "npm:express@4.18.2";
 import format from "npm:pg-format";
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
@@ -14,72 +14,71 @@ const db = new Client(Deno.env.get("DB_CONN_STR"));
 //* interfaces
 
 //* models
-const checkFollows = async (username): Promise<void> => {
-  const formattedQuery = format(
-    `SELECT following FROM users
+const checkFollows = async (username: string) => {
+	const formattedQuery = format(
+		`SELECT following FROM users
     WHERE username = '%s'
     ;`,
-    username
-  );
-  const { rows } = await db.queryObject(formattedQuery);
-  return rows[0];
+		username
+	);
+	const { rows } = await db.queryObject(formattedQuery);
+	return rows[0] as { following: string[] };
 };
 
-const updateFollows = async (username, newFollows): Promise<void> => {
-  const formattedQuery = format(
-    `UPDATE users
+const updateFollows = async (username: string, newFollows: string[]) => {
+	const formattedQuery = format(
+		`UPDATE users
     SET following = '{%s}'
     WHERE username = '%s'
     ;`,
-    newFollows,
-    username
-  );
-  const { rows } = await db.queryObject(formattedQuery);
-  return rows as Review[];
+		newFollows,
+		username
+	);
+	const { rows } = await db.queryObject(formattedQuery);
+	return rows;
 };
 
 //* controllers
 const getFollows = async (req: Request, res: Response, next: NextFunction) => {
-  const { username } = req.params;
-  try {
-    const following = await checkFollows(username);
-    res.status(200).send(following);
-  } catch (err) {
-    next(err);
-  }
+	const { username } = req.params;
+	try {
+		const following = await checkFollows(username);
+		res.status(200).send(following);
+	} catch (err) {
+		next(err);
+	}
 };
 
 const addOrRemoveFriend = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-  const { new_follow, follow_request } = req.body;
-  const { username } = req.params;
-  try {
-    const { following } = await checkFollows(username);
-    console.log("🚀 ~ following:", following)
-    if (!following.includes(new_follow) && follow_request) {
-      const newFollows = [...following, new_follow];
-      await updateFollows(username, newFollows);
-      res
-        .status(200)
-        .send({ msg: `${username} is now following ${new_follow}` });
-    }
-    else if (following.includes(new_follow) && !follow_request) {
-      const newFollows = following.filter((user) => {
-        return user !== new_follow;
-      });
-      console.log("🚀 ~ newFollows ~ newFollows:", newFollows)
+	const { new_follow, follow_request } = req.body;
+	const { username } = req.params;
+	try {
+		const { following } = await checkFollows(username);
+		console.log("🚀 ~ following:", following);
+		if (!following.includes(new_follow) && follow_request) {
+			const newFollows = [...following, new_follow];
+			await updateFollows(username, newFollows);
+			res.status(200).send({
+				msg: `${username} is now following ${new_follow}`,
+			});
+		} else if (following.includes(new_follow) && !follow_request) {
+			const newFollows = following.filter((user: string) => {
+				return user !== new_follow;
+			});
+			console.log("🚀 ~ newFollows ~ newFollows:", newFollows);
 
-      await updateFollows(username, newFollows);
-      res
-        .status(200)
-        .send({ msg: `${username} is no longer following ${new_follow}` });
-    } else res.status(400).send({ msg: "User already followed" });
-  } catch (err) {
-    next(err);
-  }
+			await updateFollows(username, newFollows);
+			res.status(200).send({
+				msg: `${username} is no longer following ${new_follow}`,
+			});
+		} else res.status(400).send({ msg: "User already followed" });
+	} catch (err) {
+		next(err);
+	}
 };
 
 //* router
@@ -88,34 +87,49 @@ const reviewRouter = Router();
 reviewRouter.route("/:username").get(getFollows).patch(addOrRemoveFriend);
 
 //* error handlers
-const handleCustomError: ErrorRequestHandler = (err, _req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  } else next(err);
+const handleCustomError: ErrorRequestHandler = (
+	err: ErrorRequestHandler,
+	_req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (err.status) {
+		res.status(err.status).send({ msg: err.msg });
+	} else next(err);
 };
 
-const handlePsqlErrors: ErrorRequestHandler = (err, _req, res, next) => {
-  switch (err.code) {
-    case "22P02":
-    case "23502":
-      res.status(400).send({ msg: "bad request" });
-      break;
-    case "23503":
-      res.status(404).send({ msg: "not found" });
-      break;
-    default:
-      next(err);
-      break;
-  }
+const handlePsqlErrors: ErrorRequestHandler = (
+	err: ErrorRequestHandler,
+	_req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	switch (err.code) {
+		case "22P02":
+		case "23502":
+			res.status(400).send({ msg: "bad request" });
+			break;
+		case "23503":
+			res.status(404).send({ msg: "not found" });
+			break;
+		default:
+			next(err);
+			break;
+	}
 };
 
 const handle404 = (_req: Request, res: Response) => {
-  res.status(404).send({ msg: "incorrect path - path not found" });
+	res.status(404).send({ msg: "incorrect path - path not found" });
 };
 
-const handleServerErrors: ErrorRequestHandler = (err, _req, res, next) => {
-  console.log(err);
-  res.status(500).send({ msg: "internal server error" });
+const handleServerErrors: ErrorRequestHandler = (
+	err: ErrorRequestHandler,
+	_req: Request,
+	res: Response,
+	_next: NextFunction
+) => {
+	console.log(err);
+	res.status(500).send({ msg: "internal server error" });
 };
 
 //* listener
